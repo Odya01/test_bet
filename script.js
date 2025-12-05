60415af2fe88a6dd6f56c4460acfe2117490395b
Odya01 <republiquez25@gmail.com>
2025-12-05 13:38:24 +0300

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+// scroll animation
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll("section");
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+  const observer = new IntersectionObserver(callback, options);
+  sections.forEach((section) => {
+    section.classList.add("hidden");
+    observer.observe(section);
+  });
+});
